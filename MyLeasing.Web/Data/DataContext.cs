70ee1590fe7738da80596758155313a341291998
mdiazgrizzuti <martin.grizzuti@int-it.com>
2020-11-13 09:58:30 -0300

    DB Created

--- MyLeasing.Web/Data/DataContext.cs
+++ MyLeasing.Web/Data/DataContext.cs
@@ -8,10 +8,21 @@ namespace MyLeasing.Web.Data
     /// </summary>
     public class DataContext : DbContext
     {
-        public DbSet<Owner> Owners { get; set; }
-
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
         }
+
+        public DbSet<Owner> Owners { get; set; }
+
+        public DbSet<Contract> Contracts { get; set; }
+
+        public DbSet<Lessee> Lessees { get; set; }
+
+        public DbSet<Property> Properties { get; set; }
+
+        public DbSet<PropertyImage> PropertyImages { get; set; }
+
+        public DbSet<PropertyType> PropertyTypes { get; set; }
+
     }
 }
--- MyLeasing.Web/Data/Entities/Owner.cs
+++ MyLeasing.Web/Data/Entities/Owner.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MyLeasing.Web.Data.Entities
 {
@@ -10,34 +7,41 @@ namespace MyLeasing.Web.Data.Entities
     {
         public int Id { get; set; }
 
-        [Required]
-        [MaxLength(30)]
+        [Required(ErrorMessage = "The field {0} is required")]
+        [MaxLength(30, ErrorMessage = "The field {0} cannot have more than {1} characters.")]
         public string Document { get; set; }
 
-        [Required]
-        [MaxLength(50)]
+        [Required(ErrorMessage = "The field {0} is required")]
+        [MaxLength(50, ErrorMessage = "The field {0} cannot have more than {1} characters.")]
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
-        [Required]
-        [MaxLength(50)]
+        [Required(ErrorMessage = "The field {0} is required")]
+        [MaxLength(50, ErrorMessage = "The field {0} cannot have more than {1} characters.")]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
-        [MaxLength(20)]
+        [MaxLength(20, ErrorMessage = "The field {0} cannot have more than {1} characters.")]
         [Display(Name = "Fixed Phone")]
         public string FixedPhone { get; set; }
 
-        [MaxLength(20)]
+        [MaxLength(20, ErrorMessage = "The field {0} cannot have more than {1} characters.")]
         [Display(Name = "Cell Phone")]
         public string CellPhone { get; set; }
 
-        [MaxLength(100)]
+        [MaxLength(100, ErrorMessage = "The field {0} cannot have more than {1} characters.")]
         public string Address { get; set; }
 
 
         // Propiedades de sólo lectura: no se va a mapear en la base de datos
+        [Display(Name = "Owner Name")]
         public string FullName => $"{FirstName} {LastName}";
+
+        [Display(Name = "Owner Name")]
         public string FullNameWithDocument => $"{FirstName} {LastName} - {Document}";
+
+        public ICollection<Property> Properties { get; set; }
+
+        public ICollection<Contract> Contracts { get; set; }
     }
 }
